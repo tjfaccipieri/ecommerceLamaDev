d6bebe8f09085e53cfbf564703b1ff3353eaa14c
Thiago Faccipieri <thiago.faccipieri@gmail.com>
2022-11-27 12:13:30 -0300

    Navbar in progress

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,3 +1,11 @@
+import { Outlet, createBrowserRouter, RouterProvider } from "react-router-dom";
+import Footer from "./components/footer/Footer";
+import Navbar from "./components/navbar/Navbar";
+import Home from "./pages/home/Home";
+import Product from "./pages/product/Product";
+import Products from "./pages/products/Products";
+import "/node_modules/flag-icons/css/flag-icons.min.css";
+
 const Layout = () => {
   return (
     <div className="app">
--- client/src/components/navbar/Navbar.tsx
+++ client/src/components/navbar/Navbar.tsx
@@ -1,12 +1,29 @@
 import React from 'react'
 import {CaretDown} from 'phosphor-react'
+import { Link } from 'react-router-dom'
 
 function Navbar() {
   return (
     <div className='navbar'>
     <div className="wrapper">
       <div className="left">
-        <CaretDown size={16} />
+        <div className='item'>
+          <span className="fi fi-us"></span>
+          <CaretDown />
+        </div>
+        <div className='item'>
+          <span>USD</span>
+          <CaretDown />
+        </div>
+        <div className='item'>
+          <Link to='/products/1'>Women</Link>
+        </div>
+        <div className='item'>
+          <Link to='/products/2'>Men</Link>
+        </div>
+        <div className='item'>
+          <Link to='/products/3'>Children</Link>
+        </div>
       </div>
       <div className="center"></div>
       <div className="right"></div>
@@ -16,3 +33,5 @@ function Navbar() {
 }
 
 export default Navbar
+
+// stoped at 18:16 from https://www.youtube.com/watch?v=BCkWFblNLKU&ab_channel=LamaDev
