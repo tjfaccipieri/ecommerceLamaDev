a5f6db169df4603dc095c24df2a87f77814461e2
Thiago Faccipieri <thiago.faccipieri@gmail.com>
2022-12-03 18:51:46 -0300

    changed .env to VITE pattern

--- client/src/components/card/Card.tsx
+++ client/src/components/card/Card.tsx
@@ -15,16 +15,16 @@ function Card({ item }: CardProps) {
           <img src={item.img} alt="" className="mainImage" />
           <img src={item.img2} alt="" className="secondImage" />
         </div>
-        <h2>{item.title}</h2>
+        <h2>{item.attributes?.title}</h2>
         <div className="prices">
           <s className='oldPrice'><h3>{new Intl.NumberFormat('en-US', {
             style: 'currency',
             currency: 'USD'
-          }).format(item.oldPrice)}</h3></s>
+          }).format(item.attributes?.price +10)}</h3></s>
           <h3>{new Intl.NumberFormat('pt-br', {
             style: 'currency',
             currency: 'BRL'
-          }).format(item.price)}</h3>
+          }).format(item.attributes?.price)}</h3>
         </div>
       </div>
     </Link>
